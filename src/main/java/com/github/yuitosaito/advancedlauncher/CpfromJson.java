b768d4638304c525141abf08fbbcb1fa4fcb7b69
yuitosaito <yuitosaitou@gmail.com>
2019-12-07 11:36:06 +0900

    ver 0.01 Not available for MAC

---
+++ src/main/java/com/github/yuitosaito/advancedlauncher/CpfromJson.java
@@ -0,0 +1,70 @@
+package com.github.yuitosaito.advancedlauncher;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CpfromJson {
+
+	public static String cp(String versionid,String minecraftdir) {
+		String JsonData = "";
+		String res = "";
+        try {
+            // ファイルのパスを指定する
+            File file = new File(minecraftdir+"/versions/" + versionid + "/" + versionid + ".json");
+
+            // ファイルが存在しない場合に例外が発生するので確認する
+            if (!file.exists()) {
+                System.out.print("No File");
+                return null;
+            }
+
+            // BufferedReaderクラスのreadLineメソッドを使って1行ずつ読み込み表示する
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String data;
+            while ((data = bufferedReader.readLine()) != null) {
+                JsonData = JsonData + data;
+            }
+
+            // 最後にファイルを閉じてリソースを開放する
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+		String MinecraftDir = "C:/Users/琉球サクセス/AppData/Roaming/.minecraft";
+		JSONObject jo = new JSONObject(JsonData);
+		JSONArray ja = jo.getJSONArray("libraries");
+		int len = ja.length();
+		String s = ((JSONObject)ja.get(0)).get("name").toString();
+		int index = s.indexOf(":");
+		s = s.substring(0, index).replace(".", "/")+"/"+s.substring(index+1,s.length());
+		index = s.indexOf(":");
+		s = "libraries/"+s.substring(0, index)+"/"+s.substring(index+1,s.length());
+		File[] fa = new File(MinecraftDir + "/" + s).listFiles();
+		res = s + "/" + fa[0].getName();
+		for(int i = 1; i<len; ++i) {
+			s = ((JSONObject)ja.get(i)).get("name").toString();
+			index = s.indexOf(":");
+			s = s.substring(0, index).replace(".", "/")+"/"+s.substring(index+1,s.length());
+			index = s.indexOf(":");
+			s = "libraries/"+s.substring(0, index)+"/"+s.substring(index+1,s.length());
+			fa = new File(MinecraftDir + "/" + s).listFiles();
+
+			if(new File(MinecraftDir + "/" + s).exists()) {
+				res += ";";
+			res += s + "/" + fa[0].getName();
+			}
+		}
+		return res;
+	}
+
+}
---
+++ src/main/java/com/github/yuitosaito/advancedlauncher/Logout.java
@@ -0,0 +1,84 @@
+package com.github.yuitosaito.advancedlauncher;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+
+public class Logout extends JFrame {
+
+	private JPanel contentPane;
+	public static JTextArea textArea = new JTextArea();
+	public static JScrollPane scrollPane = new JScrollPane();
+	public static Logout frame = new Logout();
+	/**
+	 * Launch the application.
+	 */
+	public static void LogoutStart(ProcessBuilder p) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		Log log = new Log(p);
+		log.start();
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Logout() {
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		textArea.setEditable(false);
+
+
+		scrollPane.setViewportView(textArea);
+	}
+
+
+}
+
+class Log extends Thread{
+	public static ProcessBuilder p;
+Log(ProcessBuilder p){
+	Log.p = p;
+}
+	@Override
+	public void run() {
+        try {
+			// プロセスを開始する
+			Process process = p.start();
+
+			// 結果を受け取る
+			try (BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("Shift-JIS")))) {
+			    String line;
+			    while ((line = r.readLine()) != null) {
+			        Logout.textArea.setText(Logout.textArea.getText() + "\r\n" + line);
+			        Logout.scrollPane.getVerticalScrollBar().setValue(Logout.scrollPane.getVerticalScrollBar().getMaximum());
+			    }
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
