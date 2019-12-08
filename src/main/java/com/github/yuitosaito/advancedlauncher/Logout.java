abbcc0947c144cac9c9f6b412c79609f58c6f07c
anatawa12 <anatawa12@icloud.com>
2019-12-08 20:31:57 +0900

    reformat

--- src/main/java/com/github/yuitosaito/advancedlauncher/Logout.java
+++ src/main/java/com/github/yuitosaito/advancedlauncher/Logout.java
@@ -15,70 +15,73 @@ import javax.swing.border.EmptyBorder;
 
 public class Logout extends JFrame {
 
-	private JPanel contentPane;
-	public static JTextArea textArea = new JTextArea();
-	public static JScrollPane scrollPane = new JScrollPane();
-	public static Logout frame = new Logout();
-	/**
-	 * Launch the application.
-	 */
-	public static void LogoutStart(ProcessBuilder p) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+    private JPanel contentPane;
+    public static JTextArea textArea = new JTextArea();
+    public static JScrollPane scrollPane = new JScrollPane();
+    public static Logout frame = new Logout();
 
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		Log log = new Log(p);
-		log.start();
-	}
+    /**
+     * Launch the application.
+     */
+    public static void LogoutStart(ProcessBuilder p) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
 
-	/**
-	 * Create the frame.
-	 */
-	public Logout() {
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Log log = new Log(p);
+        log.start();
+    }
 
-
-		contentPane.add(scrollPane, BorderLayout.CENTER);
-		textArea.setEditable(false);
+    /**
+     * Create the frame.
+     */
+    public Logout() {
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
 
 
-		scrollPane.setViewportView(textArea);
-	}
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+        textArea.setEditable(false);
+
+
+        scrollPane.setViewportView(textArea);
+    }
 
 
 }
 
-class Log extends Thread{
-	public static ProcessBuilder p;
-Log(ProcessBuilder p){
-	Log.p = p;
-}
-	@Override
-	public void run() {
+class Log extends Thread {
+    public static ProcessBuilder p;
+
+    Log(ProcessBuilder p) {
+        Log.p = p;
+    }
+
+    @Override
+    public void run() {
         try {
-			// プロセスを開始する
-			Process process = p.start();
+            // プロセスを開始する
+            Process process = p.start();
 
-			// 結果を受け取る
-			try (BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("Shift-JIS")))) {
-			    String line;
-			    while ((line = r.readLine()) != null) {
-			        Logout.textArea.setText(Logout.textArea.getText() + "\r\n" + line);
-			        Logout.scrollPane.getVerticalScrollBar().setValue(Logout.scrollPane.getVerticalScrollBar().getMaximum());
-			    }
-			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
+            // 結果を受け取る
+            try (BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("Shift-JIS")))) {
+                String line;
+                while ((line = r.readLine()) != null) {
+                    Logout.textArea.setText(Logout.textArea.getText() + "\r\n" + line);
+                    Logout.scrollPane.getVerticalScrollBar().setValue(Logout.scrollPane.getVerticalScrollBar().getMaximum());
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 }
